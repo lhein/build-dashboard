b8db11ac6fd52b24a7cf5899b968bcb43fc5ced4
Lars Heinemann <lhein.smx@gmail.com>
2019-07-24 09:34:39 +0200

    fixed rest call and processing
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import Dashboard from './Dashboard';
 import * as serviceWorker from './serviceWorker';
 
-let response = fetch('http://0.0.0.0:50005/ci-jobs/api/v1.0/jobs/');
-let myJson = response.json(); //extract JSON from the http response
-let jobs=JSON.parse(myJson);
 
-//{% raw %}
-ReactDOM.render(<Dashboard jobs={ jobs } />, document.getElementById('root'));
-//{% endraw %}
+fetch('http://0.0.0.0:50005/ci-jobs/api/v1.0/jobs/')
+	.then(function(response) {
+		return response.json();
+  	})
+  	.then(function(myJson) {
+		console.log(JSON.stringify(myJson));
+		let jobs = myJson.jobs;
+		ReactDOM.render(<Dashboard jobs={ jobs } />, document.getElementById('root'));
+  	});
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
