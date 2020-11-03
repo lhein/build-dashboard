03534f002159ac2150def2b5b85e1faef75e78db
Lars Heinemann <lhein.smx@gmail.com>
2020-11-03 15:16:32 +0100

    fixed wrong var assignment
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- CIUtils.py
+++ CIUtils.py
@@ -84,7 +84,7 @@ def getJenkinsJobStatus(serverUrl, jobName):
 	try:
 		jobUrl = serverUrl + '/job/Fuse/job/VSCode/job/' + jobName
 		if (serverUrl != NEW_DEVTOOLS_JENKINS):
-			jobApiUrl = serverUrl + '/job/' + jobName + '/api/json'
+			jobUrl = serverUrl + '/job/' + jobName + '/api/json'
 		jobApiUrl = jobUrl + '/api/json'
 		jobStatus = fetchJenkinsStatus(jobApiUrl)
 		color = jobStatus['color']
