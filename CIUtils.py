e1225c4b4b81729b69817e5154660d48d506da2e
Lars Heinemann <lhein.smx@gmail.com>
2021-03-15 16:32:21 +0100

    FUSETOOLS2-1026 - updated Jenkins references for both QE and ENG jenkins instances
    Signed-off-by: Lars Heinemann <lhein.smx@gmail.com>

--- CIUtils.py
+++ CIUtils.py
@@ -3,53 +3,52 @@ import json
 import requests
 import time
 
-ENV_VAR_NAME = 'TRAVIS_TOKEN'
+ENV_VAR_NAME = 'CIRCLECI_TOKEN'
 ENV_VAR_UNDEFINED = 'UNDEFINED'
 
-FUSE_JENKINS = 'https://fusesource-jenkins.rhev-ci-vms.eng.rdu2.redhat.com'
 FUSE_QE_JENKINS = 'https://master-jenkins-csb-fusetools-qe.cloud.paas.psi.redhat.com'
-DEVTOOLS_JENKINS = 'https://dev-platform-jenkins.rhev-ci-vms.eng.rdu2.redhat.com'
-NEW_DEVTOOLS_JENKINS = 'https://studio-jenkins-csb-codeready.cloud.paas.psi.redhat.com'
-TRAVIS_API_HOST = 'https://api.travis-ci.com'
-TRAVIS_HOST = 'https://travis-ci.com/'
-TRAVIS = 'Travis'
+DEVTOOLS_JENKINS = 'https://studio-jenkins-csb-codeready.cloud.paas.psi.redhat.com'
+CIRCLECI_API_HOST = 'https://api.travis-ci.com'
+CIRCLECI_HOST = 'https://travis-ci.com/'
+CIRCLECI = 'Travis'
 JENKINS = 'Jenkins'
+JENKINS_FUSE_VSCODE_FOLDER = '/job/Fuse/job/VSCode/job/'
 
 JOBS = [
-	{ 'jobName': 'jbosstools-fuse_master', 						          'ci': FUSE_JENKINS, 								                'type': JENKINS },
-	{ 'jobName': 'jbosstools-fuse.sonar_master', 				        'ci': FUSE_JENKINS, 								                'type': JENKINS },
-	{ 'jobName': 'jbosstools-fuse_PullRequest', 				        'ci': FUSE_JENKINS, 								                'type': JENKINS },
-	{ 'jobName': 'jbosstools-fuse_PullRequest-OnlyTemplates',   'ci': FUSE_JENKINS, 								                'type': JENKINS },
-	{ 'jobName': 'jbosstools-fuse-extras_master', 				      'ci': DEVTOOLS_JENKINS, 						                'type': JENKINS },
-	{ 'jobName': 'jbosstools-fuse-extras-Pull-Request', 		    'ci': DEVTOOLS_JENKINS, 						                'type': JENKINS },
-	{ 'jobName': 'vscode-camel-lsp-release', 				            'ci': NEW_DEVTOOLS_JENKINS, 							          'type': JENKINS },
-	{ 'jobName': 'vscode-camel-lsp-extension-pack-release', 	  'ci': NEW_DEVTOOLS_JENKINS, 							          'type': JENKINS },
-  { 'jobName': 'vscode-atlasmap-release', 					          'ci': NEW_DEVTOOLS_JENKINS, 							          'type': JENKINS },
-  { 'jobName': 'vscode-camelk-release', 						          'ci': NEW_DEVTOOLS_JENKINS, 							          'type': JENKINS },
-  { 'jobName': 'vscode-didact-release', 					            'ci': NEW_DEVTOOLS_JENKINS, 							          'type': JENKINS },
-  { 'jobName': 'vscode-wsdl2rest-release', 					          'ci': NEW_DEVTOOLS_JENKINS, 							          'type': JENKINS },
-	{ 'jobName': 'camel-language-server', 						          'ci': 'camel-tooling/camel-language-server', 		    'type': TRAVIS },
-	{ 'jobName': 'camel-lsp-client-eclipse', 					          'ci': 'camel-tooling/camel-lsp-client-eclipse',     'type': TRAVIS },
-	{ 'jobName': 'camel-lsp-client-vscode', 					          'ci': 'camel-tooling/camel-lsp-client-vscode', 	    'type': TRAVIS },
-	{ 'jobName': 'camel-lsp-client-atom',						            'ci': 'camel-tooling/camel-lsp-client-atom',		    'type': TRAVIS },
-	{ 'jobName': 'vscode-wsdl2rest', 							              'ci': 'camel-tooling/vscode-wsdl2rest', 			      'type': TRAVIS },
-	{ 'jobName': 'vscode-atlasmap', 							              'ci': 'jboss-fuse/vscode-atlasmap', 				        'type': TRAVIS },
-	{ 'jobName': 'vscode-camelk', 								              'ci': 'camel-tooling/vscode-camelk', 				        'type': TRAVIS },
-	{ 'jobName': 'vscode-camel-extension-pack', 				        'ci': 'camel-tooling/vscode-camel-extension-pack', 	'type': TRAVIS },
-	{ 'jobName': 'vscode-atlasmap-pipeline', 					          'ci': FUSE_QE_JENKINS, 								              'type': JENKINS },
-	{ 'jobName': 'vscode-camelk-pipeline', 						          'ci': FUSE_QE_JENKINS, 								              'type': JENKINS },
-	{ 'jobName': 'vscode-lsp-pipeline', 						            'ci': FUSE_QE_JENKINS, 								              'type': JENKINS },
-	{ 'jobName': 'vscode-wsdl2rest-pipeline', 					        'ci': FUSE_QE_JENKINS, 								              'type': JENKINS },
-	{ 'jobName': 'vscode-project-initializer-pipeline', 		    'ci': FUSE_QE_JENKINS, 								              'type': JENKINS },
-	{ 'jobName': 'lsp-eclipse-client-nightly-matrix', 			    'ci': FUSE_QE_JENKINS, 								              'type': JENKINS },
-  { 'jobName': 'fuse-smoke', 									                'ci': FUSE_QE_JENKINS, 							                'type': JENKINS }
+	{ 'jobName': 'jbosstools-fuse_master', 						          'ci': DEVTOOLS_JENKINS,   					                'folder': '/job/Studio/job/Engineering/job/build_master/job/',   'type': JENKINS },
+  { 'jobName': 'jbosstools-fuse-jdknext_master',              'ci': DEVTOOLS_JENKINS,   					                'folder': '/job/Studio/job/Engineering/job/build_jdkNext/job/',  'type': JENKINS },
+	{ 'jobName': 'pullrequest-fuse', 	            			        'ci': DEVTOOLS_JENKINS,   					                'folder': '/job/Studio/job/Engineering/job/pr_checks/job/',      'type': JENKINS },
+  { 'jobName': 'jbosstools-fuse-extras_master', 				      'ci': DEVTOOLS_JENKINS,			    		                'folder': '/job/Studio/job/Engineering/job/build_master/job/',   'type': JENKINS },
+  { 'jobName': 'jbosstools-fuse-extras-jdknext_master',       'ci': DEVTOOLS_JENKINS,   					                'folder': '/job/Studio/job/Engineering/job/build_jdkNext/job/',  'type': JENKINS },
+	{ 'jobName': 'pullrequest-fuse-extras',              		    'ci': DEVTOOLS_JENKINS,					                    'folder': '/job/Studio/job/Engineering/job/pr_checks/job/',      'type': JENKINS },
+  { 'jobName': 'vscode-atlasmap-release', 					          'ci': DEVTOOLS_JENKINS, 							              'folder': JENKINS_FUSE_VSCODE_FOLDER,                            'type': JENKINS },
+	{ 'jobName': 'vscode-camel-lsp-extension-pack-release', 	  'ci': DEVTOOLS_JENKINS, 							              'folder': JENKINS_FUSE_VSCODE_FOLDER,                            'type': JENKINS },
+  { 'jobName': 'vscode-camel-lsp-release', 				            'ci': DEVTOOLS_JENKINS, 						    	          'folder': JENKINS_FUSE_VSCODE_FOLDER,                            'type': JENKINS },
+  { 'jobName': 'vscode-camelk-release', 						          'ci': DEVTOOLS_JENKINS, 							              'folder': JENKINS_FUSE_VSCODE_FOLDER,                            'type': JENKINS },
+  { 'jobName': 'vscode-didact-release', 					            'ci': DEVTOOLS_JENKINS, 							              'folder': JENKINS_FUSE_VSCODE_FOLDER,                            'type': JENKINS },
+  { 'jobName': 'vscode-wsdl2rest-release', 					          'ci': DEVTOOLS_JENKINS, 							              'folder': JENKINS_FUSE_VSCODE_FOLDER,                            'type': JENKINS },
+	{ 'jobName': 'camel-language-server', 						          'ci': 'camel-tooling/camel-language-server', 		    'folder': '/',                                                   'type': CIRCLECI },
+	{ 'jobName': 'camel-lsp-client-eclipse', 					          'ci': 'camel-tooling/camel-lsp-client-eclipse',     'folder': '/',                                                   'type': CIRCLECI },
+	{ 'jobName': 'camel-lsp-client-vscode', 					          'ci': 'camel-tooling/camel-lsp-client-vscode', 	    'folder': '/',                                                   'type': CIRCLECI },
+	{ 'jobName': 'camel-lsp-client-atom',						            'ci': 'camel-tooling/camel-lsp-client-atom',		    'folder': '/',                                                   'type': CIRCLECI },
+	{ 'jobName': 'vscode-wsdl2rest', 							              'ci': 'camel-tooling/vscode-wsdl2rest', 			      'folder': '/',                                                   'type': CIRCLECI },
+	{ 'jobName': 'vscode-atlasmap', 							              'ci': 'jboss-fuse/vscode-atlasmap', 				        'folder': '/',                                                   'type': CIRCLECI },
+	{ 'jobName': 'vscode-camelk', 								              'ci': 'camel-tooling/vscode-camelk', 				        'folder': '/',                                                   'type': CIRCLECI },
+	{ 'jobName': 'vscode-camel-extension-pack', 				        'ci': 'camel-tooling/vscode-camel-extension-pack', 	'folder': '/',                                                   'type': CIRCLECI },
+	{ 'jobName': 'vscode-atlasmap-pipeline', 					          'ci': FUSE_QE_JENKINS, 								              'folder': '/job/vscode/job/',                                    'type': JENKINS },
+	{ 'jobName': 'vscode-camelk-pipeline', 						          'ci': FUSE_QE_JENKINS, 								              'folder': '/job/vscode/job/',                                    'type': JENKINS },
+	{ 'jobName': 'vscode-lsp-pipeline', 						            'ci': FUSE_QE_JENKINS, 								              'folder': '/job/vscode/job/',                                    'type': JENKINS },
+	{ 'jobName': 'vscode-wsdl2rest-pipeline', 					        'ci': FUSE_QE_JENKINS, 								              'folder': '/job/vscode/job/',                                    'type': JENKINS },
+	{ 'jobName': 'vscode-project-initializer-pipeline', 		    'ci': FUSE_QE_JENKINS, 								              'folder': '/job/vscode/job/',                                    'type': JENKINS },
+	{ 'jobName': 'lsp-eclipse-client-nightly-matrix', 			    'ci': FUSE_QE_JENKINS, 								              'folder': '/job/vscode/job/',                                    'type': JENKINS },
+  { 'jobName': 'fuse-smoke', 									                'ci': FUSE_QE_JENKINS, 							                'folder': '/job/',                                               'type': JENKINS }
 ]
 
-def getTravisToken():
+def getCircleCIToken():
 	return os.getenv(ENV_VAR_NAME, ENV_VAR_UNDEFINED)
 
-def hasTravisTokenDefined():
-	return getTravisToken() != ENV_VAR_UNDEFINED
+def hasCircleCITokenDefined():
+	return getCircleCIToken() != ENV_VAR_UNDEFINED
 
 def mapToJenkinsStates(buildResult):
 	mappedState = ''
@@ -67,8 +66,8 @@ def mapToJenkinsStates(buildResult):
 
 	return mappedState
 
-def fetchTravisStatus(url):
-	token = getTravisToken()
+def fetchCircleCIStatus(url):
+	token = getCircleCIToken()
 
 	response = requests.get(url, headers={'User-Agent': 'CI-Dashboard', 'Travis-API-Version': '3' ,'Authorization': 'token ' + token}, verify=False, timeout=5)
 	jobStatus = json.loads(response.text)
@@ -80,11 +79,9 @@ def fetchJenkinsStatus(url):
 	jobStatus = json.loads(response.text)
 	return jobStatus
 
-def getJenkinsJobStatus(serverUrl, jobName):
+def getJenkinsJobStatus(serverUrl, folderName, jobName):
 	try:
-		jobUrl = serverUrl + '/job/Fuse/job/VSCode/job/' + jobName
-		if (serverUrl != NEW_DEVTOOLS_JENKINS):
-			jobUrl = serverUrl + '/job/' + jobName + '/api/json'
+		jobUrl = serverUrl + folderName + jobName
 		jobApiUrl = jobUrl + '/api/json'
 		jobStatus = fetchJenkinsStatus(jobApiUrl)
 		color = jobStatus['color']
@@ -101,23 +98,22 @@ def getJenkinsJobStatus(serverUrl, jobName):
 	except Exception as e:
 		return { 'name': jobName, 'buildNumber': 'UNKNOWN', 'buildStatus': 'UNKNOWN', 'buildUrl': '', 'jobUrl': jobUrl}
 
-def getTravisJobStatus(repo, jobName):
+def getCircleCIJobStatus(repo, folderName, jobName):
 	try:
 		parts = repo.split('/')
-		branch = fetchTravisStatus(TRAVIS_API_HOST + '/repo/github/' + parts[0] + '%2F' + parts[1] + '/branch/master')
+		branch = fetchCircleCIStatus(CIRCLECI_API_HOST + '/repo/github/' + parts[0] + '%2F' + parts[1] + '/branch/master')
 		lastBuild = branch['last_build']
 		buildId = lastBuild['id']
 		buildResult = mapToJenkinsStates(lastBuild['state'])
 		last_build_number = lastBuild['number']
 
-		jobUrl = TRAVIS_HOST + 'github/' + repo + '/builds/'
+		jobUrl = CIRCLECI_HOST + 'github/' + repo + '/builds/'
 		buildLink = jobUrl + str(buildId)
-
 
 		return { 'name': jobName, 'buildNumber': str(last_build_number), 'buildStatus': buildResult, 'buildUrl': buildLink, 'jobUrl': jobUrl }
 	except Exception as e:
 		print(e)
-		return { 'name': jobName, 'buildNumber': 'UNKNOWN', 'buildStatus': 'UNKNOWN', 'buildUrl': '', 'jobUrl': TRAVIS_HOST + repo + '/builds/' }
+		return { 'name': jobName, 'buildNumber': 'UNKNOWN', 'buildStatus': 'UNKNOWN', 'buildUrl': '', 'jobUrl': CIRCLECI_HOST + repo + '/builds/' }
 
 def getJobSkeleton(jobName):
 	return { 'name': jobName, 'buildNumber': 'UNKNOWN', 'buildStatus': 'UNKNOWN', 'buildUrl': '', 'jobUrl': '' }
@@ -129,9 +125,9 @@ def getJob(jobName):
 	job = list(filter(lambda job: job['jobName'] == jobName, JOBS))[0]
 
 	if job['type'] == JENKINS:
-		jobStatus = getJenkinsJobStatus(job['ci'], jobName)
-	elif job['type'] == TRAVIS:
-		jobStatus = getTravisJobStatus(job['ci'], jobName)
+		jobStatus = getJenkinsJobStatus(job['ci'], job['folder'], jobName)
+	elif job['type'] == CIRCLECI:
+		jobStatus = getCircleCIJobStatus(job['ci'], job['folder'], jobName)
 	else:
 		jobStatus = None
 
